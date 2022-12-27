20bee0543519bed8aaf73f47ce7e564fd96e50a7
unknown <dragontaek98@naver.com>
2022-12-27 18:09:29 +0900

    feat: add BOJ11651

---
+++ BOJ/Yongtaek/BOJ11651.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class BOJ11651 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int arr[][] = new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            for(int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(arr, (m1, m2) -> {
+            if(m1[1] == m2[1]) {
+                return m1[0] - m2[0];
+            } else {
+                return m1[1] - m2[1];
+            }
+        });
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 2; j++){
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+        
+    }
+}
