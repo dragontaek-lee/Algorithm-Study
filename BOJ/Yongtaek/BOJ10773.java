d50463d6aee3d83d79acdec3bbc16c25170276e9
unknown <dragontaek98@naver.com>
2022-12-30 02:57:09 +0900

    feat: add BOJ10773

---
+++ BOJ/Yongtaek/BOJ10773.java
@@ -0,0 +1,31 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10773 {
+    static int[] stack;
+    static int size = -1;
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        stack = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0){
+                if(size != -1) size--;
+            } else {
+                size++;
+                stack[size] = num;
+            }
+        }
+
+        int sum = 0;
+        for(int j = 0; j < size+1; j++) {
+            sum += stack[j];
+        }
+
+        System.out.println(sum);
+    }
+}
