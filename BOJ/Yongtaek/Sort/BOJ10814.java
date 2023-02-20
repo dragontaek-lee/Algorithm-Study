e2fc927ad44f4fa79ae98686f65d477c8c42aff4
yongtaeklee <dragontaek98@naver.com>
2023-02-20 13:00:20 +0900

    refactor: code BOJ10814

--- BOJ/Yongtaek/Sort/BOJ10814.java
+++ BOJ/Yongtaek/Sort/BOJ10814.java
@@ -1,38 +1,14 @@
 package Sort;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 import java.io.BufferedReader;
+import java.util.Collections;
 
 public class BOJ10814 {
-    public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-
-        Person arr[] = new Person[n];
-
-        for(int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            arr[i] = new Person(Integer.parseInt(st.nextToken()), st.nextToken());
-        }
-
-        Arrays.sort(arr, new Comparator<Person>() {
-            public int compare(Person p1, Person p2) {
-                return p1.age - p2.age;
-            }
-        });
-
-        for(int i = 0; i < n ; i++) {
-            sb.append(arr[i].age + " " + arr[i].name + "\n");
-        }
-
-        System.out.println(sb);
-    }
-
     public static class Person {
         int age;
         String name;
@@ -41,5 +17,30 @@ public class BOJ10814 {
             this.age = age;
             this.name = name;
         }
+    }
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Person> arr = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr.add(new Person(Integer.parseInt(st.nextToken()), st.nextToken()));
+        }
+
+        Collections.sort(arr, new Comparator<Person>() {
+            public int compare(Person p1, Person p2) {
+                return p1.age - p2.age;
+            }   
+        });
+
+        for(int i = 0; i < n ; i++) {
+            sb.append(arr.get(i).age + " " + arr.get(i).name + "\n");
+        }
+
+        System.out.println(sb);
     }
 }
