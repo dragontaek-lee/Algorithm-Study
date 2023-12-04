5d798b0fa181d09b5d81c628df79c66f63ae71f6
YongtaekLee <54241139+dragontaek-lee@users.noreply.github.com>
2023-12-04 22:14:05 +0900

    [level 2] Title: 구명보트, Time: 8.45 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int index = 0;
+        
+        for (int i = people.length-1; i >= 0; i--) {
+            if (index > i) break;
+            if (people[i] + people[index] <= limit) {
+                index++;
+                answer++;
+            } else {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
