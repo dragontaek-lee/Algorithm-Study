682ea4c150b1bbbcdbcac924da6b3a8f03615222
YongtaekLee <54241139+dragontaek-lee@users.noreply.github.com>
2024-01-10 18:06:08 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.54 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[60001];
+        
+        dp[0] = 1;
+        dp[1] = 2;
+        
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+        
+        return dp[n-1];
+    }
+}
